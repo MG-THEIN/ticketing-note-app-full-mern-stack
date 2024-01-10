2466ca06c846e0dda6d49b92090dd9c40845e8b8
MG-THEIN <mt4130c@gmail.com>
2024-01-10 18:57:37 -0500

    Backend ready with Access token and refresh token

--- Frontend_MERN_Stack_Project/src/features/notes/NewNote.js
+++ Frontend_MERN_Stack_Project/src/features/notes/NewNote.js
@@ -1,12 +1,14 @@
-import { useSelector } from 'react-redux'
-import { selectAllUsers } from '../users/usersApiSlice'
-import NewNoteForm from './NewNoteForm'
+import { useSelector } from "react-redux"
+import { selectAllUsers } from "../users/usersApiSlice"
+import NewNoteForm from "./NewNoteForm"
 
 const NewNote = () => {
-    const users = useSelector(selectAllUsers)
+  const users = useSelector(selectAllUsers)
 
-    const content = users ? <NewNoteForm users={users} /> : <p>Loading...</p>
+  if (!users?.length) return <p>Not Currently Availabel</p>
 
-    return content
+  const content = <NewNoteForm users={users} />
+
+  return content
 }
 export default NewNote
