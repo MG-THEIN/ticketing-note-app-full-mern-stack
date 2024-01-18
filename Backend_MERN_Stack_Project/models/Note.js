c53e9a9235100ee1dbcb7495bf4fdef489b9cf74
MG-THEIN <mt4130c@gmail.com>
2024-01-18 17:53:42 -0500

    JWT Tokens and cookies are all applied securely and all the routes are protected depending on the roles of hte users

--- Backend_MERN_Stack_Project/models/Note.js
+++ Backend_MERN_Stack_Project/models/Note.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose")
-//const AutoIncrement = require("mongoose-sequence")(mongoose)
+const AutoIncrement = require("mongoose-sequence")(mongoose)
 
 const noteSchema = new mongoose.Schema(
   {
@@ -25,11 +25,11 @@ const noteSchema = new mongoose.Schema(
     timestamps: true,
   }
 )
-/*
+
 noteSchema.plugin(AutoIncrement, {
   inc_field: "ticket",
   id: "ticketNums",
   start_seq: 500,
-}) */
+})
 
 module.exports = mongoose.model("Note", noteSchema)
--- Frontend_MERN_Stack_Project/src/App.js
+++ Frontend_MERN_Stack_Project/src/App.js
@@ -12,34 +12,45 @@ import EditNote from "./features/notes/EditNote"
 import NewNote from "./features/notes/NewNote"
 import Prefetch from "./features/auth/Prefetch"
 import PersistLogin from "./features/auth/PersistLogin"
+import { ROLES } from "./config/roles"
+import RequiredAuth from "./features/auth/RequiredAuth"
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
+        {/* public routes */}
         <Route index element={<Public />} />
         <Route path="login" element={<Login />} />
-
+        {/* protected routes */}
         <Route element={<PersistLogin />}>
-          <Route element={<Prefetch />}>
-            <Route path="dash" element={<DashLayout />}>
-              <Route index element={<Welcome />} />
+          <Route
+            element={<RequiredAuth allowedRoles={[...Object.values(ROLES)]} />}>
+            <Route element={<Prefetch />}>
+              <Route path="dash" element={<DashLayout />}>
+                <Route index element={<Welcome />} />
+                <Route
+                  element={
+                    <RequiredAuth allowedRoles={[ROLES.Manager, ROLES.Admin]} />
+                  }>
+                  <Route path="users">
+                    <Route index element={<UsersList />} />
+                    <Route path=":id" element={<EditUser />} />
+                    <Route path="new" element={<NewUserForm />} />
+                  </Route>
+                </Route>
 
-              <Route path="users">
-                <Route index element={<UsersList />} />
-                <Route path=":id" element={<EditUser />} />
-                <Route path="new" element={<NewUserForm />} />
+                <Route path="notes">
+                  <Route index element={<NotesList />} />
+                  <Route path=":id" element={<EditNote />} />
+                  <Route path="new" element={<NewNote />} />
+                </Route>
               </Route>
-
-              <Route path="notes">
-                <Route index element={<NotesList />} />
-                <Route path=":id" element={<EditNote />} />
-                <Route path="new" element={<NewNote />} />
-              </Route>
+              {/* End Dash */}
             </Route>
-            {/* End Dash */}
           </Route>
-        </Route>
+        </Route>{" "}
+        {/* End protected routes */}
       </Route>
     </Routes>
   )
